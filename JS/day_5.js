6abdbdc610cf22ac9a1d6f3ab45c9578d85ab123
AkkalDhami <dhamiakkal21@gmail.com>
2024-09-13 20:29:25 +0545

    updated day 5 of logic building code

--- JS/day_5.js
+++ JS/day_5.js
@@ -5,7 +5,7 @@ let arr = [12, 2, 3, 45, 6];
 let smallest = arr[0];
 let largest = arr[0];
 
-for (let i = 0; i < arr.length - 1; i++) {
+for (let i = 0; i < arr.length; i++) {
   if (arr[i] < smallest) {
     smallest = arr[i];
   }
@@ -16,3 +16,15 @@ for (let i = 0; i < arr.length - 1; i++) {
 console.log("Smallest: " + smallest);
 console.log("Largest: " + largest);
 // Output: smallest = 2, largest = 45
+
+
+//* Alternative Method
+function findLargest(arr) {
+  return Math.max(...arr);
+}
+function findSmallest(arr) {
+  return Math.min(...arr);
+}
+
+console.log("Smallest: " + findSmallest([1, 2, 3, 4, 5, 6, 7])); // 1
+console.log("Largest: " + findLargest([2, 23, 34, 45, 56, 67])); // 67
