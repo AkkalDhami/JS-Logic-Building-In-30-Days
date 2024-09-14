7cd0463cfab5105e7c6884566b8e2a00a7f32a6d
AkkalDhami <dhamiakkal21@gmail.com>
2024-09-14 19:39:58 +0545

    sum of all elements in an array

--- JS/day_6.js
+++ JS/day_6.js
@@ -1,5 +1,6 @@
+//? Find the sum of all elements in an array.
 
-let arr = [1, 2, 3, 4, 5];
+let arr = [1, 2, 3, 4, 5, 6];
 
 function sumArray(arr) {
 
@@ -11,11 +12,11 @@ function sumArray(arr) {
     return sum;
 }
 
-console.log(sumArray(arr)); 
+console.log(sumArray(arr)); // 21
 
 //* Alternative method
 
 function sumArr(arr) {
     return arr.reduce((acc, val) => acc + val, 0);  // ES6+ syntax
 }
-console.log(sumArr(arr));
+console.log(sumArr(arr)); // 21
