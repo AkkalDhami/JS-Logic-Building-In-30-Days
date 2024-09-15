6ac808d31cfe369693fb97d88e2c5593c8c5df34
AkkalDhami <dhamiakkal21@gmail.com>
2024-09-15 19:19:30 +0545

    reverse a given string without using built-in methods

---
+++ JS/day_7.js
@@ -0,0 +1,10 @@
+//? Write a function to reverse a given string without using built-in methods.
+let string = "javascript"
+function reverseStr(str){
+    let reverseString = '';
+    for(let i = str.length - 1; i >=0; i--){
+        reverseString+=str[i];
+    }
+    return reverseString;
+}
+console.log(reverseStr(string)); //tpircsavaj
