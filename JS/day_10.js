a4bd8efb7465743b42cf47e62c04fa10ec0d4355
Aavash Dhami <dhamiakkal21@gmail.com>
2024-09-19 19:05:29 +0545

    finds and prints duplicate elements in an array

--- JS/day_10.js
+++ JS/day_10.js
@@ -1,28 +1,26 @@
 // Write a program that finds and prints duplicate elements in an array.
 
+let duplicates = [];
 function findDuplicates(arr) {
-    let duplicates = [];
-    let seen = {};
-  
-    for (let i = 0; i < arr.length; i++) {
-      if (seen[arr[i]]) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[i] === arr[j] && !duplicates.includes(arr[i])) {
         duplicates.push(arr[i]);
-      } else {
-        seen[arr[i]] = true;
       }
     }
-  
-    return duplicates;
   }
-  
-  console.log(findDuplicates([1, 2, 3, 2, 4, 5, 6, 4])); // [2, 4]
+  return duplicates;
+}
+
+console.log(findDuplicates(arr));
   
 
 //Alternative Method 
   
-  let arr = [1, 2, 3, 4, 5, 2, 3, 6, 7, 8, 9, 10];
+let arr = [1, 2, 3, 4, 5, 2, 3, 6, 7, 8, 9, 10];
 let dupes = [
   ...new Set(arr.filter((x) => arr.filter((y) => y === x).length > 1)),
 ];
 
 console.log(dupes); // [2, 3]
+
