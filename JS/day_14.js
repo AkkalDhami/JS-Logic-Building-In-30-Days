cb5aa6bbd7fe35df295012c85367b707ebd418af
AkkalDhami <dhamiakkal21@gmail.com>
2024-09-22 18:52:24 +0545

    shorten a string to a specific length

---
+++ JS/day_14.js
@@ -0,0 +1,11 @@
+//? Writa a finction that shorten a string to a specific length and add ... at the end.
+
+function shortenString(str, maxLength) {
+  if (str.length > maxLength) {
+    return str.slice(0, maxLength) + "...";
+  } else {
+    return str;
+  }
+}
+
+console.log(shortenString("Hello World", 10)); // "Hello Worl..."
