018c402ca5d55bd9c3b42cb291c936a665db9d2e
Aavash Dhami <dhamiakkal21@gmail.com>
2024-09-24 19:18:32 +0545

    returns the sum of all multiples of 3 or 5

---
+++ JS/day_16.js
@@ -0,0 +1,19 @@
+//? Write a function that returns the sum of all multiples of 3 or 5 below a given number.
+let sumProduct = (num) => {
+  let result = 0;
+  for (let i = 1; i < num; i++)
+    if (i % 3 == 0 || i % 5 == 0) {
+      result += i;
+    }
+  return result;
+};
+console.log(sumProduct(10)); // Output: 23 (3 + 5 + 6 + 9)
+
+//? ALTERNATIVE METHOD using ES6
+
+const sumProduct2 = (num) =>
+  [...Array(num).keys()]
+    .filter((i) => i % 3 === 0 || i % 5 === 0)
+    .reduce((acc, i) => acc + i, 0);
+
+console.log(sumProduct2(10)); // Output: 23 (3 + 5 + 6 + 9)
